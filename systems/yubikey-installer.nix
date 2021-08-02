72a623b236753715bd4bcea40e43ba9d69fed622
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-08-02 07:22:51 -0400

    refactor(livecd): make the installation cd minimal, and use the latest kernel

--- systems/yubikey-installer.nix
+++ systems/yubikey-installer.nix
@@ -4,9 +4,11 @@ let
   config = { pkgs, ... }:
     with pkgs; {
       imports = [
-        <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-plasma5.nix>
+        <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
         <nixos-hardware/dell/xps/13-9310>
       ];
+
+      boot.kernelPackages = pkgs.linuxPackages_latest;
 
       services.pcscd.enable = true;
       services.udev.packages = [ yubikey-personalization ];
