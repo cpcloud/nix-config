a915cd0dd40f6ae61cf9e862100f5e52e02ae99f
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-08-04 11:41:16 -0400

    feat(ssh): clean up ssh config

--- users/cloud/trusted/ssh.nix
+++ users/cloud/trusted/ssh.nix
@@ -1,24 +1,28 @@
-{
-  home.file.".ssh/config".text = ''
-    Host github.com
-        ControlMaster auto
-        IdentityFile ~/.ssh/id_rsa_yubikey.pub
-        User git
-    Host *
-        AddKeysToAgent yes
-        ChallengeResponseAuthentication no
-        ControlMaster auto
-        ControlPath ~/.ssh/a-%C
-        ControlPersist 30m
-        ForwardAgent no
-        ForwardX11 no
-        ForwardX11Trusted no
-        HashKnownHosts yes
-        IdentitiesOnly yes
-        IdentityAgent /run/user/1000/gnupg/S.gpg-agent.ssh
-        ServerAliveCountMax 5
-        ServerAliveInterval 60
-        StrictHostKeyChecking ask
-        VerifyHostKeyDNS yes
-  '';
+{ lib, ... }: {
+  programs.ssh = {
+    enable = true;
+    forwardAgent = false;
+    hashKnownHosts = true;
+    controlMaster = "auto";
+    controlPath = "~/.ssh/a-%C";
+    controlPersist = "30m";
+    extraConfig = ''
+      IdentityAgent /run/user/1000/gnupg/S.gpg-agent.ssh
+      ChallengeResponseAuthentication no
+      AddKeysToAgent yes
+      StrictHostKeyChecking ask
+      VerifyHostKeyDNS yes
+      IdentitiesOnly yes
+      ServerAliveCountMax 5
+      ServerAliveInterval 60
+    '';
+
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        identityFile = "~/.ssh/id_rsa_yubikey.pub";
+        user = "git";
+      };
+    };
+  };
 }
