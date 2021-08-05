08c43c3f96035d7d1c3ce292a477d511d433d0f5
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-08-05 08:47:08 -0400

    feat(yubikey): add nfc key

--- users/cloud/trusted/ssh.nix
+++ users/cloud/trusted/ssh.nix
@@ -2,7 +2,6 @@
   home.file.".ssh/config".text = ''
     Host github.com
         ControlMaster no
-        IdentityFile ~/.ssh/id_rsa_yubikey.pub
         User git
     Host *
         AddKeysToAgent yes
@@ -16,6 +15,8 @@
         HashKnownHosts yes
         IdentitiesOnly yes
         IdentityAgent /run/user/1000/gnupg/S.gpg-agent.ssh
+        IdentityFile ~/.ssh/id_rsa_yubikey_5_nano.pub
+        IdentityFile ~/.ssh/id_rsa_yubikey_5_nfc.pub
         ServerAliveCountMax 5
         ServerAliveInterval 60
         StrictHostKeyChecking ask
