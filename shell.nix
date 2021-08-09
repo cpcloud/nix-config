9e7af5cd1bed07280e6821f6022ec1f729104aac
Phillip Cloud <cpcloud@gmail.com>
2021-08-09 08:08:01 -0400

    style: pull out sops-nix into a variable

--- shell.nix
+++ shell.nix
@@ -1,13 +1,12 @@
 let
   sources = import ./nix;
   pkgs = import sources.nixpkgs { };
+  sops-nix = pkgs.callPackage sources.sops-nix { };
 in
 pkgs.mkShell {
   name = "nix-config";
-  nativeBuildInputs = [
-    (pkgs.callPackage sources.sops-nix { }).sops-import-keys-hook
-  ];
-  buildInputs = (with pkgs; [
+  nativeBuildInputs = [ sops-nix.sops-import-keys-hook ];
+  buildInputs = with pkgs; [
     cryptsetup
     gnupg
     jq
@@ -18,7 +17,7 @@ pkgs.mkShell {
     srm
     ssh-to-pgp
     yj
-  ]);
+  ];
 
   sopsPGPKeyDirs = [
     "./keys/hosts"
