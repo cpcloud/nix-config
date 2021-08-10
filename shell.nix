0c8c52da1c82796c92e31525c3926c837539a33a
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-08-09 21:57:56 -0400

    ci: add commit getting script

--- shell.nix
+++ shell.nix
@@ -1,13 +1,26 @@
 let
   sources = import ./nix;
-  pkgs = import sources.nixpkgs { };
+  pkgs = import sources.nixpkgs {
+    overlays = [
+      (import ./overlays/write-sane-shell-script-bin.nix)
+    ];
+  };
   sops-nix = pkgs.callPackage sources.sops-nix { };
+  get-commit = pkgs.writeSaneShellScriptBin {
+    name = "get-commit";
+    buildInputs = with pkgs; [ jq ];
+    src = ''
+      jq --exit-status --arg dep "$1" -rcM \
+      '.[$dep].rev // error("no niv-managed \"\($dep)\" dependency")' < ${./nix/sources.json}
+    '';
+  };
 in
 pkgs.mkShell {
   name = "nix-config";
   nativeBuildInputs = [ sops-nix.sops-import-keys-hook ];
   buildInputs = with pkgs; [
     cryptsetup
+    get-commit
     gnupg
     jq
     niv
