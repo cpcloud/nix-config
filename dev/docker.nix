2ee1994d3ad5aba66525bd1753fa4a9d59d3e04c
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-08-30 17:21:13 -0400

    refactor: conditionally enable docker things

--- dev/docker.nix
+++ dev/docker.nix
@@ -1,16 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
   json = pkgs.formats.json { };
+  enableDocker = true;
 in
 {
   # enable docker
-  virtualisation.docker.enable = true;
+  virtualisation.docker.enable = enableDocker;
 
   # this is needed for docker builds that contact the internet, gross
-  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  boot.kernel.sysctl = lib.optionalAttrs enableDocker {
+    "net.ipv4.ip_forward" = 1;
+  };
 
   # allow use of the GCR mirror of docker images
-  environment.etc."docker/daemon.json".source = json.generate "docker_daemon.json" {
-    registry-mirrors = [ "https://mirror.gcr.io" ];
+  environment.etc = lib.optionalAttrs enableDocker {
+    "docker/daemon.json".source = json.generate "docker_daemon.json" {
+      registry-mirrors = [ "https://mirror.gcr.io" ];
+    };
   };
 }
