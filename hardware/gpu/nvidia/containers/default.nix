8793eb4d0c40e15b7cb5004258e3cb957eb8f95b
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-09-09 07:29:26 -0400

    fix: fix nvidia container testing

--- hardware/gpu/nvidia/containers/default.nix
+++ hardware/gpu/nvidia/containers/default.nix
@@ -7,10 +7,16 @@ let
   # are enabled
   tensorflowImage = "tensorflow/tensorflow:${pkgs.python3Packages.tensorflow.version}-gpu";
   cudaImage = "nvidia/cuda:${pkgs.cudatoolkit_11.version}-base-ubi7";
+
   pythonScript = ''
     import tensorflow as tf
-    assert tf.config.list_physical_devices(\"GPU\")
+
+    devices = tf.config.list_physical_devices(\"GPU\")
+    assert devices, \"no GPU devices found\"
+
+    print(\"\n\".join(map(repr, devices)))
   '';
+
   testNVidiaContainerSimple = runtime: pkgs.writeSaneShellScriptBin {
     name = "test_nvidia_${runtime}_simple";
     buildInputs = [ pkgs.${runtime} ];
@@ -20,33 +26,36 @@ let
       "${runtime}" run --runtime nvidia --rm "${cudaImage}" nvidia-smi --list-gpus
     '';
   };
+
   testNVidiaContainerTensorFlow = runtime: pkgs.writeSaneShellScriptBin {
     name = "test_nvidia_${runtime}_tensorflow";
     buildInputs = [ pkgs.${runtime} ];
     src = ''
       set -x
 
-      podman run \
-      -e TF_CPP_MIN_LOG_LEVEL=1 \
-      -e LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu \
-      --runtime nvidia \
-      --rm \
-      ${tensorflowImage} \
-      python -c "${pythonScript}"
+      ${runtime} \
+        run \
+        -e TF_CPP_MIN_LOG_LEVEL=1 \
+        -e LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu \
+        --runtime nvidia \
+        --rm \
+        ${tensorflowImage} \
+        python -c "${pythonScript}"
     '';
   };
+
+  genScripts = runtime: [
+    (testNVidiaContainerSimple runtime)
+    (testNVidiaContainerTensorFlow runtime)
+  ];
 in
 {
-  # this parameter is necessary because the nvidia container runtime odoesn't
-  # support cgroupsv2
-  boot.kernelParams = lib.optionals enableNvidiaContainerRuntime [ "systemd.unified_cgroup_hierarchy=0" ];
-
-  environment.systemPackages = lib.optionals
-    enableNvidiaDocker [
-    (testNVidiaContainerSimple "docker")
-    (testNVidiaContainerTensorFlow "docker")
-  ] ++ lib.optionals enableNvidiaPodman [
-    (testNVidiaContainerSimple "podman")
-    (testNVidiaContainerTensorFlow "podman")
+  # this kernel parameter is necessary because the nvidia container runtime
+  # odoesn't support cgroupsv2 :(
+  boot.kernelParams = lib.optionals enableNvidiaContainerRuntime [
+    "systemd.unified_cgroup_hierarchy=0"
   ];
+
+  environment.systemPackages = lib.optionals enableNvidiaDocker (genScripts "docker")
+    ++ lib.optionals enableNvidiaPodman (genScripts "podman");
 }
