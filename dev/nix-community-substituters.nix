9e9d01c0d334e908e597a2372c2d7504d43e8ab8
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-09-26 08:57:54 -0400

    refactor: add caches

--- dev/nix-community-substituters.nix
+++ dev/nix-community-substituters.nix
@@ -1,10 +1,18 @@
-{ ... }: {
+{ ... }:
+let
+  pairs = {
+    "http://nix-community.cachix.org" = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+    "https://stupidb.cachix.org" = "stupidb.cachix.org-1:iJ5Je/opEa7W23qm2CWj2s0XHK2sb8Cb8INKASK0lbY=";
+    "https://cpcloud-nix-config.cachix.org" = "cpcloud-nix-config.cachix.org-1:1c6ZPYRACVNB3cYg9A+h9Xof8MKVVj3lYdye++RWSvo=";
+    "https://nix-linter.cachix.org" = "nix-linter.cachix.org-1:BdTne5LEHQfIoJh4RsoVdgvqfObpyHO5L0SCjXFShlE=";
+    "https://ibis.cachix.org" = "ibis.cachix.org-1:tKNWCdKmBXJFK1JE/SnA41z7U7XPFOnB7Nw0vLKXaLA=";
+    "https://numbsql.cachix.org" = "numbsql.cachix.org-1:MMBbwgBZ+f+9EBvXX2ag3hVV4nRYm13UM3wGY11r81M=";
+    "https://poetry2nix.cachix.org" = "poetry2nix.cachix.org-1:2EWcWDlH12X9H76hfi5KlVtHgOtLa1Xeb7KjTjaV/R8=";
+  };
+in
+{
   nix = {
-    binaryCaches = [
-      "http://nix-community.cachix.org"
-    ];
-    binaryCachePublicKeys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+    binaryCaches = builtins.attrNames pairs;
+    binaryCachePublicKeys = builtins.attrValues pairs;
   };
 }
