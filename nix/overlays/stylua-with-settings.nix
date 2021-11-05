8884bcba42610f45cc373c4a27a9d9395186d81a
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-11-05 10:30:00 -0400

    chore: add overlays for stylua and prettier

---
+++ nix/overlays/stylua-with-settings.nix
@@ -0,0 +1,16 @@
+{ ... }: self: _: {
+  styluaWithFormat =
+    let
+      styluaSettings = builtins.fromTOML (
+        self.lib.replaceStrings [ "_" ] [ "-" ] (self.lib.readFile ../../stylua.toml)
+      );
+      styluaSettingsArgs = self.lib.concatStringsSep
+        " "
+        (self.lib.mapAttrsToList (name: value: "--${name}=${toString value}") styluaSettings);
+    in
+    self.writeShellScriptBin "stylua" ''
+      set -euo pipefail
+
+      ${self.stylua}/bin/stylua ${styluaSettingsArgs} "$@"
+    '';
+}
