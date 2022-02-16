601e08ead49ee20e8fd6e5c6ff9d1f7a0195ed7e
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-02-16 17:12:24 -0500

    chore: add more cloud toolkits

--- users/cloud/core/voltrondata.nix
+++ users/cloud/core/voltrondata.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     awscli2
+    google-cloud-sdk
+    docker-credential-gcr
     amazon-ecr-credential-helper
     ssm-session-manager-plugin
   ];
