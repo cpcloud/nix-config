68a75d95216a152207cf7c8b870b35f3a6c7f88e
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-05-06 15:24:21 -0400

    chore: remove pubkey types from openssh config

--- core/openssh.nix
+++ core/openssh.nix
@@ -6,8 +6,6 @@
     permitRootLogin = lib.mkDefault "no";
     extraConfig = ''
       StreamLocalBindUnlink yes
-      PubkeyAcceptedKeyTypes +ssh-rsa
-      HostKeyAlgorithms +ssh-rsa
     '';
   };
 }
