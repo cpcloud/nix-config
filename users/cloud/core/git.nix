b391886b40b84a5eca538d7927050d6e8ecafdeb
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-05-12 08:05:24 -0400

    style: make git and gh configs more concise

--- users/cloud/core/git.nix
+++ users/cloud/core/git.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
 
-  home.packages = with pkgs.gitAndTools; [
-    git-extras
+  home.packages = [
+    pkgs.gitAndTools.git-extras
   ];
 
   programs = {
@@ -31,9 +31,7 @@
 
     gh = {
       enable = true;
-      settings = {
-        git_protocol = "https";
-      };
+      settings.git_protocol = "https";
     };
   };
 }
