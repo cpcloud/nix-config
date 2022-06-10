ccc43d6331728a9ee08d4bb1c08a36f5955dcba8
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-06-10 07:33:54 -0500

    fix: bump disk size for gce machines

--- hardware/machines/gce/gce.nix
+++ hardware/machines/gce/gce.nix
@@ -9,7 +9,7 @@
     ../../hardware.nix
   ];
 
-  virtualisation.googleComputeImage.diskSize = 10 * 1024;
+  virtualisation.googleComputeImage.diskSize = 32 * 1024;
 
   networking = {
     useNetworkd = lib.mkForce false;
