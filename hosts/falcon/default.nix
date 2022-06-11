c4011b00d8675cf6a35785b95d9fe471771c1871
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-06-11 13:06:16 -0500

    chore: only config if docker is enabled

--- hosts/falcon/default.nix
+++ hosts/falcon/default.nix
@@ -1,4 +1,4 @@
-{ config, ... }: {
+{ config, lib, ... }: {
   imports = [
     ../../core
 
@@ -10,14 +10,15 @@
     ../../users/cloud
   ];
 
-  virtualisation.docker.daemon.settings = {
-    default-address-pools = [
-      {
-        base = "172.31.0.0/16";
-        size = 24;
-      }
-    ];
-  };
+  virtualisation.docker.daemon.settings =
+    lib.optionalAttrs config.virtualisation.docker.enable {
+      default-address-pools = [
+        {
+          base = "172.31.0.0/16";
+          size = 24;
+        }
+      ];
+    };
 
   home-manager.users.cloud = { ... }: {
     imports = [ ((import ../../users/cloud/trusted) config.networking.hostName) ];
