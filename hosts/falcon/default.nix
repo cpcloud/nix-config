1d24357f1627a9d897f3529133f7360725389097
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-06-11 13:33:07 -0500

    fix(cloud): add workaround for nixpkgs issue 109389

--- hosts/falcon/default.nix
+++ hosts/falcon/default.nix
@@ -10,15 +10,9 @@
     ../../users/cloud
   ];
 
-  virtualisation.docker.daemon.settings =
-    lib.optionalAttrs config.virtualisation.docker.enable {
-      default-address-pools = [
-        {
-          base = "172.31.0.0/16";
-          size = 24;
-        }
-      ];
-    };
+  networking.dhcpcd = lib.optionalAttrs config.virtualisation.docker.enable {
+    denyInterfaces = [ "veth*" ];
+  };
 
   home-manager.users.cloud = { ... }: {
     imports = [ ((import ../../users/cloud/trusted) config.networking.hostName) ];
