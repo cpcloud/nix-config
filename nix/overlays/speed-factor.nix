4baf5d398a8c3b39d56b6e106b090e5603d257bf
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-07-05 07:46:33 -0400

    chore: use less circuitous arithmetic

--- nix/overlays/speed-factor.nix
+++ nix/overlays/speed-factor.nix
@@ -2,7 +2,7 @@ _: _: {
   # penalize cloud hosts by a somewhat arbitrary %25
   getSpeedFactor = { builderCores, hostCores, isCloudHost }:
     let
-      adjustedBuilderCores = if isCloudHost then (builderCores / 4.0) * 3.0 else builderCores;
+      adjustedBuilderCores = if isCloudHost then builderCores * 0.75 else builderCores;
     in
     builtins.floor (adjustedBuilderCores / hostCores);
 }
