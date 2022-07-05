6f12aa1e1c8d313d617cc9d725c2cb40988b7550
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-07-05 07:18:10 -0400

    fix(deploy): call floor in `getSpeedFactor`
    This is so that cloud hosts with an equal number of cores as native hosts have a lower speed factor

--- nix/overlays/speed-factor.nix
+++ nix/overlays/speed-factor.nix
@@ -2,7 +2,7 @@ _: _: {
   # penalize cloud hosts by a somewhat arbitrary %25
   getSpeedFactor = { builderCores, hostCores, isCloudHost }:
     let
-      hostCores = if isCloudHost then (builderCores / 4) * 3 else builderCores;
+      hostCores = if isCloudHost then (builderCores / 4.0) * 3.0 else builderCores;
     in
-    builderCores / hostCores;
+    builtins.floor (builderCores / hostCores);
 }
