7a33af3b3afe2e50a80622c678a9e8cf6142c3c8
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-07-14 06:07:44 -0400

    chore: remove `aws` from `index.ts`

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import * as aws from "./aws";
 import * as gcp from "./gcp";
 import * as p from "@pulumi/pulumi";
 import { Stack } from "./core";
@@ -6,7 +5,7 @@ import { Stack } from "./core";
 export = (): Record<string, p.Output<string>> => {
   const dev = new p.Config("dev");
 
-  const modules = { aws, gcp };
+  const modules = { gcp };
   const outputs: Record<string, p.Output<string>> = {};
 
   for (const [key, module] of Object.entries(modules)) {
